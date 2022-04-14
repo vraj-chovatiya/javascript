0c822833096f09393389155f3aadbbf9d7b08808
vraj-chovatiya <vrajchovatiya414@gmail.com>
2022-04-14 18:12:53 +0530

    uncomment

--- programs/basic.js
+++ programs/basic.js
@@ -7,12 +7,12 @@
 
 
 // using queryselector
-// const mainheading = document.querySelector("#main-heading");
-// const header = document.querySelector(".header");
-// const container = document.querySelector(".container"); // multiple class ne select karyu hoy to first class jene apyo hoy ae j print thay. and badha class ne access karva hoy to "querySelectorAll" method use thay.
-// console.log(mainheading);
-// console.log(header);
-// console.log(container);
+const mainheading = document.querySelector("#main-heading");
+const header = document.querySelector(".header");
+const container = document.querySelector(".container"); // multiple class ne select karyu hoy to first class jene apyo hoy ae j print thay. and badha class ne access karva hoy to "querySelectorAll" method use thay.
+console.log(mainheading);
+console.log(header);
+console.log(container);
 
 
 
@@ -20,34 +20,34 @@
 // change text
 // textcontent and innertext
 
-// const heading = document.getElementById("main-heading");
-// console.log(heading.textContent);
-// heading.textContent = "this is something else";
-// console.log(heading.textContent);
+const heading = document.getElementById("main-heading");
+console.log(heading.textContent);
+heading.textContent = "this is something else";
+console.log(heading.textContent);
 
 
 
 // change the styles of Elements 
 
-// const m_heading = document.querySelector("div.headline h2");
-// console.log(m_heading.style);
-// m_heading.style.color = "#fff";
-// m_heading.style.backgroundColor = "blue";
-// m_heading.style.padding = "10px";
-// m_heading.style.border  = "3px solid green";
+const m_heading = document.querySelector("div.headline h2");
+console.log(m_heading.style);
+m_heading.style.color = "#fff";
+m_heading.style.backgroundColor = "blue";
+m_heading.style.padding = "10px";
+m_heading.style.border  = "3px solid green";
 
 
 
 
 // get and set attribute
-// const link = document.querySelector("a");
-// console.log(link.getAttribute("href").slice(1));
-// link.setAttribute("href", "https://codeprog.com");
-// console.log(link.getAttribute("href"));
+const link = document.querySelector("a");
+console.log(link.getAttribute("href").slice(1));
+link.setAttribute("href", "https://codeprog.com");
+console.log(link.getAttribute("href"));
 
 
-// const input = document.querySelector(".form-todo input");
-// console.log(input.getAttribute("type"));
+const input = document.querySelector(".form-todo input");
+console.log(input.getAttribute("type"));
 
 
 
@@ -56,12 +56,12 @@
 // get multiple elements using getElements by class name
 // get multiple items using querySelector
 
-// const navitems = document.getElementsByClassName("nav-item");  // html collection
-// console.log(typeof navitems);
-// console.log(Array.isArray(navitems));   // check navitems is a array like object or not! 
+const navitems = document.getElementsByClassName("nav-item");  // html collection
+console.log(typeof navitems);
+console.log(Array.isArray(navitems));   // check navitems is a array like object or not! 
 
-// const navitem = document.querySelectorAll(".nav-item");  // nodeList 
-// console.log(navitem);  
+const navitem = document.querySelectorAll(".nav-item");  // nodeList 
+console.log(navitem);  
 
 
 
