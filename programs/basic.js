8a7cc5496ffdcca7a6a80f752d1d1d39a119a8d1
vraj-chovatiya <vrajchovatiya414@gmail.com>
2022-04-14 17:02:09 +0530

    other file added

--- programs/basic.js
+++ programs/basic.js
@@ -7,12 +7,12 @@
 
 
 // using queryselector
-const mainheading = document.querySelector("#main-heading");
-const header = document.querySelector(".header");
-const container = document.querySelector(".container"); // multiple class ne select karyu hoy to first class jene apyo hoy ae j print thay. and badha class ne access karva hoy to "querySelectorAll" method use thay.
-console.log(mainheading);
-console.log(header);
-console.log(container);
+// const mainheading = document.querySelector("#main-heading");
+// const header = document.querySelector(".header");
+// const container = document.querySelector(".container"); // multiple class ne select karyu hoy to first class jene apyo hoy ae j print thay. and badha class ne access karva hoy to "querySelectorAll" method use thay.
+// console.log(mainheading);
+// console.log(header);
+// console.log(container);
 
 
 
@@ -29,3 +29,49 @@ console.log(container);
 
 // change the styles of Elements 
 
+// const m_heading = document.querySelector("div.headline h2");
+// console.log(m_heading.style);
+// m_heading.style.color = "#fff";
+// m_heading.style.backgroundColor = "blue";
+// m_heading.style.padding = "10px";
+// m_heading.style.border  = "3px solid green";
+
+
+
+
+// get and set attribute
+// const link = document.querySelector("a");
+// console.log(link.getAttribute("href").slice(1));
+// link.setAttribute("href", "https://codeprog.com");
+// console.log(link.getAttribute("href"));
+
+
+// const input = document.querySelector(".form-todo input");
+// console.log(input.getAttribute("type"));
+
+
+
+
+
+// get multiple elements using getElements by class name
+// get multiple items using querySelector
+
+// const navitems = document.getElementsByClassName("nav-item");  // html collection
+// console.log(typeof navitems);
+// console.log(Array.isArray(navitems));   // check navitems is a array like object or not! 
+
+// const navitem = document.querySelectorAll(".nav-item");  // nodeList 
+// console.log(navitem);  
+
+
+
+
+
+
+// loop 
+const navitems = document.getElementsByClassName("nav-item");  // html collection
+console.log(typeof navitems);
+console.log(Array.isArray(navitems));   // check navitems is a array like object or not! 
+
+const navitem = document.querySelectorAll(".nav-item");  // nodeList 
+console.log(navitem);  
